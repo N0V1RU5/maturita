1ec9eadb9c500f35f4e6508960b27ec229c87b21
N0V1 <petema@delta-studenti.cz>
2022-10-16 14:12:03 +0200

    First commit
    První commit projektu

---
+++ Assets/Scripts/BatteryPickUp.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickUp : MonoBehaviour
+{
+    private bool inReach;
+
+    public GameObject pickUpText;
+    private GameObject flashlight;
+
+    //public AudioSource pickUpSound;
+
+    void Start()
+    {
+        inReach = false;
+        pickUpText.SetActive(false);
+        flashlight = GameObject.FindGameObjectWithTag("flashlight");
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("interact") && inReach)
+        {
+            flashlight.GetComponent<FlashlightAdvanced>().batteries += 1;
+            //pickUpSound.Play();
+            inReach = false;
+            pickUpText.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FlashlightAdvanced.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FlashlightAdvanced : MonoBehaviour
+{
+    public new Light light;
+    public TMP_Text text;
+
+    public TMP_Text batteryText;
+
+    public float lifetime;
+    public float batteries;
+    public float FDuration;
+    public float range = 100f;
+    public float damage = 1f;
+
+    public Camera fpsCam;
+
+    //public AudioSource flashON;
+    //public AudioSource flashOFF;
+
+    private bool on;
+    private bool off;
+
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+
+        off = true;
+        light.enabled = false;
+    }
+
+    void Update()
+    {
+        text.text = lifetime.ToString("0") + "%";
+        batteryText.text = batteries.ToString();
+
+        if (Input.GetButtonDown("flashlight") && off)
+        {
+            //flashON.Play();
+            light.enabled = true;
+            on = true;
+            off = false;
+        }
+
+        else if (Input.GetButtonDown("flashlight") && on)
+        {
+            //flashOFF.Play();
+            light.enabled = false;
+            on = false;
+            off = true;
+        }
+
+        if (on)
+        {
+            lifetime -= 1 * Time.deltaTime;
+            light.intensity = lifetime / 200;
+        }
+
+        if (lifetime <= 0 && batteries > 0)
+        {
+            batteries -= 1;
+            lifetime += 100 - FDuration;
+        }
+
+        if (lifetime <= 0 && batteries <= 0)
+        {
+            light.enabled = false;
+            on = false;
+            off = true;
+            lifetime = 0;
+        }
+
+        if (batteries <= 0)
+        {
+            batteries = 0;
+        }
+
+        if (Input.GetButtonDown("Fire1") && on && batteries > 0)
+        {
+            batteries -= 1;
+            Shoot();
+            lifetime = 100;
+        }
+
+        if (lifetime >= 100 - FDuration)
+        {
+            light.intensity = 100;
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
